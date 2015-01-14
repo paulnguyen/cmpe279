8ac71feaf2b665b7f4c5175916d90a6e33d1c358
paulnguyen <paul.nguyen@sjsu.edu>
2015-01-14 14:35:02 -0800

    vm

---
+++ assembly/stack/addr.asm
@@ -0,0 +1,25 @@
+
+SECTION .data
+
+str: db "this is a string"
+
+SECTION .text
+
+global _start
+
+_start:
+	nop
+
+	mov eax, [str]  ; str = displacement. address of str in eax
+	mov eax, [esp+07h] ; base + displacement -- buffer offset?	
+	mov ecx, [ebp+ebx] ; base + index  -- arrays?
+
+	nop
+
+ 	mov eax, 1      ; exit system call
+        mov ebx, 0      ; return code
+        int 80H         ; exit
+
+SECTION .bss
+
+      
