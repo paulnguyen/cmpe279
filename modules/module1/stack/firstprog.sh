1b7f7b48de2e1955a74859dce006404e47df2dda
paulnguyen <paul.nguyen@sjsu.edu>
2015-01-19 23:55:07 -0800

    vm

--- modules/module1/stack/firstprog.sh
+++ modules/module1/stack/firstprog.sh
@@ -1,3 +1,3 @@
-gcc -g firstprog.c -o firstprog
-objdump -D firstprog > firstprog-objdump-att.txt
-objdump -M intel -D firstprog > firstprog-objdump-intel.txt
+gcc -g firstprog.c -o firstprog.out
+objdump -D firstprog.out > firstprog-objdump-att.txt
+objdump -M intel -D firstprog.out > firstprog-objdump-intel.txt
