6fd50d55d9ed258fda739be577971f2b9e5a87c2
Paul Nguyen <paul.nguyen@sjsu.edu>
2015-01-19 23:42:16 -0800

    source re-org to modules

---
+++ modules/module2/stack/callstack.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+void A( int tmp ) ;
+void B( int a, int b ) ;
+void C( ) ;
+
+int main(int argc, char **argv)
+{
+	printf("hello world\n");
+	A(1) ;
+	return 0;
+}
+
+void A( int tmp )
+{
+	if ( tmp<2 )
+		B(1,2) ;
+}
+
+void B(int a, int b) {
+	C() ;
+}
+
+void C() {
+	A(2) ;
+}
