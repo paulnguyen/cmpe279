ed6c70a0adb5d68b279253b7ad7bef0c58349ffc
paulnguyen <paul.nguyen@sjsu.edu>
2015-01-21 01:47:26 -0800

    vm

--- modules/module2/securec/gets.c
+++ modules/module2/securec/gets.c
@@ -1,11 +1,16 @@
-  #include <stdio.h>
-  #include <stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-  void get_y_or_n(void) {
+int main() {
+  get_y_or_n() ;
+  return 0 ;
+}
+
+void get_y_or_n(void) {
     char response[8];
     puts("Continue? [y] n: ");
     gets(response);
     if (response[0] == 'n')
       exit(0);
     return;
-  }
+}
