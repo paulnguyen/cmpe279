deea17074cbafcc01c815206e2ca7e79a9d1a6a2
Paul Nguyen <paul.nguyen@sjsu.edu>
2015-01-21 11:41:00 -0800

    mod2

--- modules/module2/hacking/datatype_sizes.c
+++ modules/module2/hacking/datatype_sizes.c
@@ -8,4 +8,8 @@ int main() {
 	printf("The 'long long int' data type is %d bytes\n", sizeof(long long int));
 	printf("The 'float' data type is\t %d bytes\n", sizeof(float));
 	printf("The 'char' data type is\t\t %d bytes\n", sizeof(char));
+
+ 	printf( "sizeof(int*) = %lu\n", sizeof(int*)) ;
+    printf( "sizeof(int)  = %lu\n", sizeof(int)) ;
+    printf( "sizeof(int*)==sizeof(int) = %d\n", sizeof(int*)==sizeof(int)) ;
 }
