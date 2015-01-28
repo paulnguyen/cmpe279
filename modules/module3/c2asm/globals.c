73f179db96c2ecb31b52b8ecaf2f2af3afb95ec8
Paul Nguyen <paul.nguyen@sjsu.edu>
2015-01-28 12:14:34 -0800

    .

---
+++ modules/module3/c2asm/globals.c
@@ -0,0 +1,10 @@
+
+int x = 1;
+int y = 2;
+
+void main()
+{
+   x = x+y;
+   printf("Total = %d\n", x);
+}
+
---
+++ modules/module3/c2asm/locals.c
@@ -0,0 +1,9 @@
+
+void main()
+{
+   int x = 1;
+   int y = 2;
+
+   x = x+y;
+   printf("Total = %d\n", x);
+}
